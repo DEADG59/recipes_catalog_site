28f2f69d0fc3419bbc2f0fda8e993cd92c5e1b14
DEADG59 <maks-akulov-03@mail.ru>
2025-05-16 02:37:06 +0600

    add recipes\urls.py AND update cuisine\urls.py views.py
    add view recipe_list recipe_detail

--- cuisine/recipes/views.py
+++ cuisine/recipes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-# Create your views here.
+
+def recipe_list(request):
+    recipes = Recipe.published.all()
+    return render(request,
+                  'recipes/recipe/list.html',
+                  {'recipes': recipes})
+
+
+def recipe_detail(request, id):
+    recipe = get_object_or_404(Recipe,
+                               id=id,
+                               status=Recipe.Status.PUBLISHED)
+    cuisines = Recipe.published.get(id=id).cuisine.all()
+    return render(request,
+                  'recipes/recipe/detail.html',
+                  {'recipe': recipe,
+                   'cuisines': cuisines})
