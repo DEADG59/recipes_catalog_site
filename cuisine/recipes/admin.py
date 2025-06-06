f3293c59a8e2b166f116e4d0277f0995f78d29c3
DEADG59 <maks-akulov-03@mail.ru>
2025-06-06 22:48:16 +0600

    update admin.py
    add TabularInline cuisine in recipe

--- cuisine/recipes/admin.py
+++ cuisine/recipes/admin.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from .models import *
 
 
+class CuisineInline(admin.TabularInline):
+    model = Recipe.cuisine.through
+
+
 @admin.register(Cuisine)
 class CuisineAdmin(admin.ModelAdmin):
+    inlines = [
+        CuisineInline,
+    ]
+
     list_display = ['title']
     search_fields = ['title']
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        CuisineInline,
+    ]
+    exclude = ['cuisine']
     list_display = ['title', 'slug', 'author', 'publish', 'status']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'description']
