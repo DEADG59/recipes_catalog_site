817822db2b352f91eb750859385842368131aa2e
DEADG59 <maks-akulov-03@mail.ru>
2025-06-30 16:06:42 +0600

    add sitemaps.py AND update settings.py urls.py
    add sitemap

--- cuisine/cuisine/urls.py
+++ cuisine/cuisine/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from recipes.sitemaps import RecipeSitemap
+
+
+sitemaps = {
+    'recipes': RecipeSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipes/', include('recipes.urls', namespace='recipes')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
